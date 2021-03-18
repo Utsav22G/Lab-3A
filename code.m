c67d8a0c9f26a5448a3cb72e8a1e084b852bcb81
Kristtiya <kristtiyaguerra3.14@gmail.com>
2021-03-17 22:03:42 -0400

    Modified Code.mlx.

--- code.m
+++ code.m
@@ -1,18 +1,12 @@
 clc; clear all; close all;
 
 % Creating X_k
-size_X = 5000;
+size_X = 6400;
 range = [-1 1];
 X_k = randi(range,size_X,1);
 
 % IDFT
 x_m = ifft(X_k); %Turns data points into impulses 
-
-
-% Adding cyclic prefix
-cycPrefix = x_m(49:64);
-x_L_m = [x_m(49:64); x_m];
-
 
 % Extending the 64 points
 L = 64; %Length of Pulse
@@ -28,14 +22,40 @@ x_0(1:L:end) = x_m;
 
 % now convolve the single generic pulse with the spread-out bits
 x_m_pulse = conv(pulse, x_0);
+x_m_pulse= x_m_pulse(1:409600);
+
+figure;
+stem(x_m_pulse)
+ylabel('amplitude')
+
+
+% Adding cyclic prefix
+d = 64;
+n_data = size_X / d;
+
+Len_pulse_data = length(x_m_pulse) + length(x_m_pulse)/4;
+
+x_L_m = [x_m_pulse(3073:4096); x_m_pulse];
+for R = 2:n_data
+    cycPrefix = x_m_pulse(3136+(R-1)*4096:4096+(R-1)*4096);
+    end_unit = d*L*1*(R-1) + 16*L*(R-1);
+    x_L_m = [x_L_m(1: end_unit); x_L_m(end_unit + (R-1)*4096 : end_unit + (R-1)*4096) ; x_L_m(end_unit + 5120:end)];
+end
+
+% a = [1,2,4,5];
+%    b = [a(1:2) 3 a(3:end)]
+
 
 % Transmit data
-y_m = nonflat_channel(x_L_m);
+y_m = nonflat_channel(x_m_pulse);
+
+
+
 
 % IDFT
 Y_k = fft(y_m,L); 
-
-stem(Y_k)
+figure;
+stem(Y_k);
 %%
 
 hold on
