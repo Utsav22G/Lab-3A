614371e8a281bfcf7f10f1acb84a55ad1d473d96
Kristtiya <kristtiyaguerra3.14@gmail.com>
2021-03-22 03:45:13 -0400

    Modifying code.m: Fixed Indexing of Precyclic Data, Ran Data through Channel

--- code.m
+++ code.m
@@ -28,6 +28,7 @@ figure;
 stem(x_m_pulse)
 ylabel('amplitude')
 
+%% 
 
 % Adding cyclic prefix
 d = 64;
@@ -35,30 +36,39 @@ n_data = size_X / d;
 
 Len_pulse_data = length(x_m_pulse) + length(x_m_pulse)/4;
 
-x_L_m = [x_m_pulse(3073:4096); x_m_pulse];
-for R = 2:n_data
-    cycPrefix = x_m_pulse(3136+(R-1)*4096:4096+(R-1)*4096);
-    end_unit = d*L*1*(R-1) + 16*L*(R-1);
-    x_L_m = [x_L_m(1: end_unit); x_L_m(end_unit + (R-1)*4096 : end_unit + (R-1)*4096) ; x_L_m(end_unit + 5120:end)];
+%Index 1st cyclic prefix
+A = x_m_pulse(3073: 4096);
+x_m_load(1:1024) = A;
+x_m_load(1025:5120) = x_m_pulse(1: 4096);
+
+for R = 2:1:100
+    cycPrefix = x_m_pulse(4096*(R-1)+ 64*48+1: 4096*(R));
+    x_m_load(80*64*(R-1)+1 : 5120*(R-1) + 1024) = cycPrefix;
+    Post_chunk =  x_m_pulse(4096*(R-1)+1: 4096*(R));
+    x_m_load(5120*(R-1) + 1024+1 : 5120*(R-1) + 1024 +4096) = Post_chunk;
+
 end
 
-% a = [1,2,4,5];
-%    b = [a(1:2) 3 a(3:end)]
+stem(x_m_load)
+hold on
+title('Data with Cyclic Prefixes')
+hold off
+x_m_load = transpose(x_m_load);
+% Transmit data through nonflat channel
+y_m = nonflat_channel(x_m_load);
 
 
-% Transmit data
-y_m = nonflat_channel(x_m_pulse);
 
 
-
-
-% IDFT
-Y_k = fft(y_m,L); 
+% FFT - Fast FOurier Transform of Received Data
+Y_k = fft(y_m, L); 
 figure;
 stem(Y_k);
-%%
-
 hold on
-stem(X_k)
+title('Received Data')
+hold off
+%%
+stem(x_m)
+hold on
 stem(Y_k)
 hold off
